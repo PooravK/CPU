197af19c4da1ab5088f3096888fcdb5d1a709685
PooravK <boomster.work@gmail.com>
2025-12-05 20:53:15 +0530

    Some changes

--- rtl/Instruction_mem.v
+++ rtl/Instruction_mem.v
@@ -1,13 +1,15 @@
 // Iska kaam hai ki ye PC ka diya hua address input lega, aur output karega ki us address pe kaunsa instruction stored hai
 // Iska asynchronous hona critical hai taki jaise hi PC ka address change hota hai, isko immediately naya instruction output krna hai
+ //32 bit registers jinki length 256 bits tak hai. Choosing 256 was purely a design choice. Width however was fixed at 32
+  // Kyuki 32 bit register hai, to next address hamesha increament of 4 me aaega. To LSB ki 2 bits hamesha 00 rahengi. Now, 256 = 2^8 isliye we went from 2 to 9 
 
 module Instruction_mem(
     input [31:0]PC_address,
     output [31:0]IM_instruction
     );
 
-    reg [31:0]mem[255:0]; //32 bit registers jinki length 256 bits tak hai. Choosing 256 was purely a design choice. Width however was fixed at 32.
+    reg [31:0]mem[255:0];
 
-    assign IM_instruction = mem[PC_address[9:2]]; // Kyuki 32 bit register hai, to next address hamesha increament of 4 me aaega. To LSB ki 2 bits hamesha 00 rahengi. Now, 256 = 2^8 isliye we went from 2 to 9 
+    assign IM_instruction = mem[PC_address[9:2]];
 
 endmodule;
--- rtl/PC.v
+++ rtl/PC.v
@@ -1,6 +1,7 @@
 // Ye agle instruction ka memory address store karke rakhta hai 
 // Clock cycle ke start me ye address instruction memory ko bhejta hai, instruction memory isse fetch karti hai ki kaunsa instruction stored hai is address par
 // Fir ye instruction rest of the CPU ko send hota hai to be decoded and executed
+// Kyuki ham 32 bits use kar rhe hai
 
 
 module PC(
@@ -18,7 +19,7 @@ module PC(
             PC_out <= 0;
         end else begin
             case (PC_op)
-                INCREAMENT: PC_out <= PC_out + 4; // Kyuki ham 32 bits use kar rhe hai
+                INCREAMENT: PC_out <= PC_out + 4;
                 LOAD: PC_out <= PC_in;
                 default: PC_out <= PC_out;
             endcase
